f274910554546b8fbaded51f509a4cf5ed3369c4
Jaime Rodriguez <jaime.rodriguez082@my.tccd.edu>
2023-08-24 21:36:06 -0500

    Initial Version

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine ();
+
+        Console.WriteLine("Hello World " + name);
+    }
+}
